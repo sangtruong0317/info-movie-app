46e7a2314ef533d15302bee42603b2e06bf623b7
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2022-06-20 12:44:07 +0700

    commit code

---
+++ src/components/Header/Heading.jsx
@@ -0,0 +1,12 @@
+import "./Heading.css";
+import mySvg from "../../images/logo1.svg.png";
+
+const Heading = () => {
+  return (
+    <div className="MainHeading">
+      <img src={mySvg} alt="" className="logo2" />{" "}
+    </div>
+  );
+};
+
+export default Heading;
